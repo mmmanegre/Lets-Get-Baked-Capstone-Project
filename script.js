8bac96da04a5afe23969a77131eb3847cdbf58a7
KayleeBroadfield <144183796+KayleeBroadfield@users.noreply.github.com>
2025-10-29 23:05:59 -0500

    Update script.js for debug logic

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Debugging
+console.log("supabase global is:", window.supabase);
+
 // Connect Supabase Project
 const SUPABASE_URL = "https://vbbxceqbzcfieymlcksu.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiYnhjZXFiemNmaWV5bWxja3N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2ODYyMDYsImV4cCI6MjA3NzI2MjIwNn0.KO4SvADxHLPW3IRPU1t_buQcY_Zim-p2G-kcK2v2Akg";
@@ -23,3 +26,5 @@ async function testConnection() {
         alert("Connection successful! Check console for results.")
     }
 }
+
+window.onload = testConnection; // or wire to a button
